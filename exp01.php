7e643431105cac34cb308cf2d8c8690c1428d96b
ryan-ladeia <145490287+ryan-ladeia@users.noreply.github.com>
2023-10-18 14:04:06 -0300

    Add files via upload

---
+++ exp01.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exp01</title>
+    <h1> Alteração teste github</h1>
+</head>
+<body>
+    <?php
+    echo " teste php";
+    ?>
+    
+</body>
+</html>
